9cc0bd975c99d5554c5a3a93577fec4a12a1ebde
Samuel <72594810+samuelpeybernes-dev@users.noreply.github.com>
2023-09-18 01:06:10 +0200

    tailwind vuetify random work

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,10 +1,20 @@
 <template>
   <DayPilotCalendar :config="config" ref="calendar" />
+  <div v-show="isLoading"
+    class="fixed h-full w-full bg-fuchsia-300 bg-opacity-25 fixed top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2">
+    <v-row>
+      <v-col cols="12" md="10" offset-md="1">
+        <v-progress-circular class="fixed top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2" indeterminate
+          color="#c026d3" width="12" size="128"></v-progress-circular>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
 import { DayPilot, DayPilotCalendar } from '@daypilot/daypilot-lite-vue'
 import { format } from 'date-fns'
+import { utcToZonedTime } from 'date-fns-tz'
 export default {
   components: {
     DayPilotCalendar,
@@ -32,6 +42,7 @@ export default {
   },
   data() {
     return {
+      isLoading: false,
       config: {
         locale: 'fr-fr',
         viewType: 'Week',
@@ -48,7 +59,7 @@ export default {
         onEventDeleted: this.onEventDeleted,
         onEventMoved: this.onEventMoved,
         onEventResized: this.onEventResized,
-        onBeforeEventRender: this.onBeforeEventRender
+        onBeforeEventRender: this.onBeforeEventRender,
       },
       combinedEvents: [],
     };
@@ -148,7 +159,7 @@ export default {
       }
     },
     async addRandomScheduleClass() {
-      const currentDate = new Date();
+      this.isLoading = true;
       const weekDays = this.getCurrentWeekDays();
 
       if (!this.hoursStore.hoursSubject || this.hoursStore.hoursSubject.length === 0) {
@@ -162,18 +173,21 @@ export default {
       const lunchBreakStartHour = 12;
       const lunchBreakEndHour = 13;
 
-      const getRandomHour = () => {
-        return Math.floor(Math.random() * (lunchBreakStartHour - 9)) + 9; // Commence entre 9h et lunchBreakStartHour
-      };
-
+      let previousSubject = null;
       const getRandomSubject = () => {
+
         const availableSubjects = this.hoursStore.hoursSubject.filter(
-          (subject) => subject.weekHours > 0
+          (subject) => subject.weekHours > 0 && subject !== previousSubject
         );
         if (availableSubjects.length === 0) {
           return null; // Toutes les matières sont déjà utilisées
         }
-        return availableSubjects[Math.floor(Math.random() * availableSubjects.length)];
+        const selectedSubject = availableSubjects[Math.floor(Math.random() * availableSubjects.length)];
+
+        // Mettez à jour la matière de l'événement précédent
+        previousSubject = selectedSubject;
+
+        return selectedSubject;
       };
 
       for (const day of weekDays) {
@@ -184,7 +198,6 @@ export default {
         startDate.setHours(9, 0, 0, 0);
 
         while (remainingEventHours > 0) {
-          const randomStartHour = getRandomHour();
           const subject = getRandomSubject();
 
           if (!subject) {
@@ -212,18 +225,20 @@ export default {
           const endDate = new Date(startDate);
           endDate.setHours(startDate.getHours() + (subject.weekHours === 0.5 ? 0 : eventDuration), startDate.getMinutes() + halfHour, 0, 0);
 
-          if (
-              (endDate.getHours() >= lunchBreakStartHour && endDate.getMinutes() > 0) || // Si l'heure de fin est après 12h01
-              endDate.getHours() === lunchBreakEndHour
-            ) {
-            const lunchBreak = lunchBreakEndHour - startDate.getHours();
-            startDate.setHours(lunchBreakEndHour, 0, 0, 0);
-            endDate.setHours(endDate.getHours() + lunchBreak, 0, 0, 0);
+          // Vérifiez si un événement d'une heure peut être ajouté entre 11h et 12h
+          if (startDate.getHours() === (lunchBreakStartHour - 1) && eventDuration > 1) {
+            // Mise à jour de l'événement actuel pour durer une heure
+            eventDuration = 1;
+            endDate.setHours(startDate.getHours() + 1, startDate.getMinutes(), 0, 0);
           }
 
+          if (this.isEventBetweenDates(startDate, endDate, lunchBreakStartHour, lunchBreakEndHour)) {
+            const lunchBreakHours = lunchBreakEndHour - startDate.getHours();
+            startDate.setHours(lunchBreakEndHour, 0, 0, 0);
+            endDate.setHours(endDate.getHours() + lunchBreakHours, endDate.getMinutes(), 0, 0);
+          }
 
-          const formatedStartDate = format(startDate, "yyyy-MM-dd'T'HH:mm:ss");
-          const formatedEndDate = format(endDate, "yyyy-MM-dd'T'HH:mm:ss");
+          const { formatedStartDate, formatedEndDate } = this.formatDate(startDate, endDate)
           // Créez un nouvel événement avec les dates générées
           const newEvent = {
             id: DayPilot.guid(),
@@ -246,16 +261,41 @@ export default {
           startDate = new Date(endDate);
         }
       }
+      this.isLoading = false;
     },
+    formatDate(startDate, endDate) {
+      const timeZone = 'Europe/Paris'
+      const zonedStartDate = utcToZonedTime(startDate, timeZone)
+      const zonedEndtDate = utcToZonedTime(endDate, timeZone)
+      const formatedStartDate = format(zonedStartDate, "yyyy-MM-dd'T'HH:mm:ss")
+      const formatedEndDate = format(zonedEndtDate, "yyyy-MM-dd'T'HH:mm:ss")
+      return { formatedStartDate, formatedEndDate }
+    },
+    isEventBetweenDates(dateDebut, dateFin, heureDebut, heureFin) {
+      const dateHeureDebut = new Date(dateDebut);
+      const dateHeureFin = new Date(dateFin);
 
+      // Utilisez les entiers pour les heures de début et de fin
+      dateHeureDebut.setHours(heureDebut, 0, 0);
+      dateHeureFin.setHours(heureFin, 0, 0);
+
+      // Vérification des heures
+      return dateHeureDebut < dateFin && dateDebut < dateHeureFin;
+    },
 
     getCurrentWeekDays() {
       const currentDate = new Date();
-      const currentDayOfWeek = currentDate.getDay(); // Jour de la semaine actuel (0 = Lundi , 1 = Mardi, ...)
-      currentDate.setDate(currentDate.getDate() - currentDayOfWeek + 1); // Réglez la date sur le lundi de la semaine actuelle
+      const currentDayOfWeek = currentDate.getDay(); // Jour de la semaine actuel (0 = Dimanche, 1 = Lundi, ...)
+
+      // Si c'est un dimanche (currentDayOfWeek === 0), ajustez la date pour obtenir la semaine actuelle
+      if (currentDayOfWeek === 0) {
+        currentDate.setDate(currentDate.getDate() - 6); // Remonter de 6 jours pour obtenir le lundi de la semaine actuelle
+      } else {
+        currentDate.setDate(currentDate.getDate() - currentDayOfWeek + 1); // Réglez la date sur le lundi de la semaine actuelle
+      }
 
       const weekDays = [];
-      for (let i = 0; i < 7; i++) {
+      for (let i = 1; i < 8; i++) {
         const day = new Date(currentDate);
         day.setDate(currentDate.getDate() + i);
         weekDays.push({
@@ -264,16 +304,13 @@ export default {
         });
       }
       return weekDays;
-
-
-    },
+    }
 
   },
 
 
   mounted: async function () {
-    //this.getCurrentWeekDays()
-    //await this.addRandomScheduleClass();
+    this.getCurrentWeekDays();
     await this.loadJobEvents();
     await this.loadClassEvents();
 
--- src/main.js
+++ src/main.js
@@ -2,8 +2,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { createPinia } from 'pinia'
+import { createVuetify  } from 'vuetify'
+import 'vuetify/dist/vuetify.min.css' 
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import './index.css'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+  })
 
 createApp(App)
+.use(vuetify)
 .use(createPinia())
 .mount('#app')
 
