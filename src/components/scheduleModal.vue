b09df2402e93f4a42431b0f2d64f239814f37f02
Samuel <samuelpeybernes33@gmail.com>
2023-10-03 17:56:27 +0200

    search color

--- src/components/scheduleModal.vue
+++ src/components/scheduleModal.vue
@@ -12,9 +12,14 @@
                     <v-card-title>
                         <span class="text-h6">Couleur de l'événement :</span>
                     </v-card-title>
+                    <v-card-text class="pb-0">
+                        <v-text-field density="compact" variant="solo" label="Search templates"
+                            append-inner-icon="mdi-magnify" single-line hide-details @update:modelValue="searchColor($event)"
+                            :model-value="search"></v-text-field>
+                    </v-card-text>
                     <v-container>
-                        <v-color-picker v-model="scheduleModal.selectedColor" mode="hex" width="auto" hide-canvas
-                            hide-inputs show-swatches></v-color-picker>
+                        <v-color-picker :swatches="filteredColors" v-model="scheduleModal.selectedColor" mode="hex"
+                            width="auto" hide-canvas hide-inputs show-swatches></v-color-picker>
                     </v-container>
                 </v-card-text>
                 <v-card-actions>
@@ -48,10 +53,11 @@ export default {
             dialogLocal: false,
             validated: false,
         }
-        
+
         return {
             scheduleModal,
-            
+            search: "",
+            filteredColors: this.formatColors(colors),
         };
     },
     methods: {
@@ -64,6 +70,46 @@ export default {
             this.scheduleModal.dialogLocal = false;
             this.$emit('canceled', this.scheduleModal);
         },
+        searchColor(event) {
+            if (event.length-1 === 0)
+                this.filteredColors = this.formatColors(colors)
+            else
+                this.filteredColors = this.filteredColorsFunc(colors, event)
+        },
+        formatColors(colors) {
+            const keys = Object.keys(colors);
+            const colorsArray = [];
+            keys.forEach((key) => {
+                const childKeys = Object.keys(colors[key]);
+                const childColors = [];
+                childKeys.forEach((childKey) => {
+                    childColors.push(colors[key][childKey]);
+                });
+                colorsArray.push(childColors);
+            });
+            return colorsArray;
+        },
+
+        filteredColorsFunc(colors, search) {
+            const keys = Object.keys(colors);
+            const colorsArray = [];
+
+            keys.forEach((key) => {
+                const childKeys = Object.keys(colors[key]);
+                childKeys.forEach((childKey) => {
+                    colorsArray.push({ parent: key, name: key + " " + childKey, color: colors[key][childKey] });
+                });
+            });
+            const colorsFiltered = colorsArray.filter(color => color.name.toLowerCase().includes(search.toLowerCase()));
+
+            const newColors = [];
+            keys.forEach(key => {
+                const child = colorsFiltered.filter(color => color.parent === key);
+                if (child.length > 0)
+                    newColors.push(child.map(color => color.color));
+            })
+            return newColors;
+        }
     },
 
 }
