f3da82685723da97735506ea01a2eb5ec126c818
Samuel <samuelpeybernes33@gmail.com>
2023-10-07 19:24:14 +0200

    title

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="my-40	">
+  <div class="my-40">
+    <v-img
+      class="mx-auto my-1"
+      max-width="100"
+      src="/calendar-96.ico"
+    ></v-img>
+    <h2 class="mb-5 text-center md:text-3xl lg:text-4xl text-transparent bg-clip-text bg-gradient-to-r from-fuchsia-800 via-fuchsia-400 to-pink-600 font-extrabold">Mael Planner</h2>
+
     <v-card class="mx-auto pa-12 pb-8" elevation="8" max-width="448" rounded="lg">
       <div class="text-subtitle-1 text-medium-emphasis">Email</div>
 
--- src/routes.js
+++ src/routes.js
@@ -1,30 +1,34 @@
-import Calendar from './components/Calendar.vue'
-import Login from './components/login.vue'
-import * as VueRouter from 'vue-router'
-import  {user} from './store/user.js'
+import Calendar from "./components/Calendar.vue";
+import Login from "./components/login.vue";
+import * as VueRouter from "vue-router";
+import { user } from "./store/user.js";
 const routes = [
-    { path: '/', name: "Calendar", component: Calendar, meta: {requiredAuth : true} },
-    { path: '/login', name: "Login", component: Login },
-    { path: '/register', name: "Register", component: Calendar },
-]
+  {
+    path: "/",
+    name: "Calendar",
+    component: Calendar,
+    meta: { requiredAuth: true },
+  },
+  { path: "/login", name: "Login", component: Login },
+  { path: "/register", name: "Register", component: Login },
+];
 
 const router = VueRouter.createRouter({
-    history: VueRouter.createWebHistory(),
-    routes,
-})
+  history: VueRouter.createWebHistory(),
+  routes,
+});
 
 router.beforeEach((to, from, next) => {
-    const userStore = user()
-    const token = localStorage.getItem('token')
-    if(token) {
-        userStore.user.auth = true
-    }
-    if(!userStore.user.auth && to.name !== 'Login' && to.meta.requiredAuth) {
-        next({name: "Login"})
-    } else {
-        next()
-    }
-})
+  const userStore = user();
+  const token = localStorage.getItem("token");
+  if (token) {
+    userStore.user.auth = true;
+  }
+  if (!userStore.user.auth && to.name !== "Login" && to.meta.requiredAuth) {
+    next({ name: "Login" });
+  } else {
+    next();
+  }
+});
 
-
-export default router
+export default router;
