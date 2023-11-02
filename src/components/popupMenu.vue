668c35dfe93be9577c5a62430c287cf664be6876
Samuel Peybernes <samuel.peybernes@compufirst.com>
2023-11-02 10:25:29 +0100

    eee

--- src/components/popupMenu.vue
+++ src/components/popupMenu.vue
@@ -1,41 +1,50 @@
 <template>
-    <div class="d-flex justify-space-around">
-        <v-menu>
-            <template v-slot:activator="{ props }">
-                <v-btn color="purple" icon="mdi-plus" v-bind="props"></v-btn>
-            </template>
-            <v-list class="text-center">
-                <v-list-item class="px-0">
-                    <v-btn height="50px" width="100%" v-on:click="openProfil = true">Profil</v-btn>
-                    <v-btn height="50px" width="100%" v-on:click="generate">Generer les cours</v-btn>
-                </v-list-item>
-            </v-list>
-        </v-menu>
-        <popupProfil :openProfil="openProfil" v-on:canceled="cancelProfil" />
-    </div>
+  <div class="d-flex justify-space-around">
+    <v-menu>
+      <template v-slot:activator="{ props }">
+        <v-btn color="purple" icon="mdi-plus" v-bind="props"></v-btn>
+      </template>
+      <v-list class="text-center">
+        <v-list-item class="px-0">
+          <v-btn height="50px" width="100%" v-on:click="openProfil = true"
+            >Profil</v-btn
+          >
+          <v-btn height="50px" width="100%" v-on:click="generate"
+            >Generer les cours</v-btn
+          >
+        </v-list-item>
+      </v-list>
+    </v-menu>
+    <popupProfil
+      :openProfil="openProfil"
+      v-on:canceled="cancelProfil"
+      v-on:validated="validateProfil"
+    />
+  </div>
 </template>
-  
+
 <script>
-import popupProfil from './popupProfil.vue';
+import popupProfil from "./popupProfil.vue";
 export default {
-    components: {
-        popupProfil,
-    },
-    data() {
-        return {
-            openProfil: false,
-        }
-    },
+  components: {
+    popupProfil,
+  },
+  data() {
+    return {
+      openProfil: false,
+    };
+  },
 
-    methods: {
-        async generate() {
-            this.$emit('generated');
-        },
-        cancelProfil() {
-            this.openProfil = this.dialogLocal;
-        },
+  methods: {
+    async generate() {
+      this.$emit("generated");
     },
-
-}
-
+    cancelProfil() {
+      this.openProfil = this.dialogLocal;
+    },
+    validateProfil() {
+      this.openProfil = this.dialogLocal;
+    },
+  },
+};
 </script>
